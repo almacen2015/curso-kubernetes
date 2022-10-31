b4f08155600a85287987d99b4c990dd8d345ca44
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2022-10-31 17:47:33 -0500

    Proyecto

---
+++ msvc-cursos/src/main/java/org/aguzman/springcloud/msvc/cursos/services/CursoServiceImpl.java
@@ -0,0 +1,61 @@
+package org.aguzman.springcloud.msvc.cursos.services;
+
+import org.aguzman.springcloud.msvc.cursos.clients.UsuarioClientRest;
+import org.aguzman.springcloud.msvc.cursos.models.Usuario;
+import org.aguzman.springcloud.msvc.cursos.models.entity.Curso;
+import org.aguzman.springcloud.msvc.cursos.repositories.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CursoServiceImpl implements CursoService{
+
+    @Autowired
+    private CursoRepository repository;
+
+    @Autowired
+    private UsuarioClientRest client;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Curso> listar() {
+        return (List<Curso>) repository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Curso> porId(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Curso guardar(Curso curso) {
+        return repository.save(curso);
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Usuario> asignarUsuario(Usuario usuario, Long cursoId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Usuario> crearUsuario(Usuario usuario, Long cursoId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Usuario> eliminarUsuario(Usuario usuario, Long cursoId) {
+        return Optional.empty();
+    }
+}
