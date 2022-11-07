b9a66bbdceaff89e749be5054fa691a4125ad510
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2022-11-07 16:59:10 -0500

    optimizacion de docker file y comunicacion entre microservicios usuarios con docker y curso de forma local

--- msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/clients/CursoClienteRest.java
+++ msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/clients/CursoClienteRest.java
@@ -3,7 +3,7 @@ package org.aguzman.springcloud.msvc.usuarios.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
-@FeignClient(name = "msvc-cursos", url = "localhost:8002")
+@FeignClient(name = "msvc-cursos", url = "host.docker.internal:8002")
 public interface CursoClienteRest {
 
     @DeleteMapping("/eliminar-curso-usuario/{id}")
